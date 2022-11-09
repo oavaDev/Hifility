74053a4ff08cee50e8d6020c469145969470968d
Omar Vega <omarvega598@gmail.com>
2022-11-09 10:44:20 -0500

    add link

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -29,9 +29,12 @@ const ProductScreen = ({ productData }) => {
 export default ProductScreen;
 
 export async function getServerSideProps() {
-  const productApi = await fetch(`http://localhost:8080/product/show`, {
-    method: 'GET',
-  });
+  const productApi = await fetch(
+    `https://hifility.herokuapp.com/product/show`,
+    {
+      method: 'GET',
+    }
+  );
   const productData = await productApi.json();
   return {
     props: {
